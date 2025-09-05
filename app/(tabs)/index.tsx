d0faa1b3a9fb85bfc3c297ba7011e7354e1fa2a2
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-09-05 16:24:20 +0530

    convex ( backend ) added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
   return (
     <View className="flex-1 justify-center items-center bg-red-300 ">
       <Text className="">Edit app/index.tsx to edit this screen.</Text>
